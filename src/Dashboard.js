227c9838eb2d2a39058dd81350ecc9f3c3a709bc
Prantar Bhowmik <prantar.bhowmik@rbc.com>
2020-06-13 23:19:31 -0400

    #6 Google materials for forms

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,9 +3,24 @@ import isEmpty from "lodash/isEmpty";
 import { useFormik } from "formik";
 import DashboardCard from "./DashboardCard";
 
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
 const { ipcRenderer } = require("electron");
 
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  }
+}));
+
 const Filter = ({ formik, setData }) => {
+  const classes = useStyles();
+
   const sortByOptions = ["Hot", "Top", "New"];
   const timeOptions = ["Hour", "Day", "Week", "Month", "Year", "All"];
   const limitOptions = [5, 10, 15, 20, 25, 50];
@@ -16,78 +31,75 @@ const Filter = ({ formik, setData }) => {
 
   return (
     <div>
-      <select
-        value={formik.values.sortBy}
-        onChange={e => {
-          formik.values.sortBy = e.target.value;
-          fetchData({
-            sortBy: e.target.value,
-            time: formik.values.time,
-            limit: formik.values.limit
-          });
-        }}
-      >
-        {sortByOptions.map(entry => {
-          return (
-            <option
-              key={entry}
-              value={entry.toLowerCase()}
-              defaultValue={entry === "Hot"}
-            >
-              {entry}
-            </option>
-          );
-        })}
-      </select>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Sort By</InputLabel>
+        <Select
+          value={formik.values.sortBy}
+          onChange={e => {
+            formik.values.sortBy = e.target.value;
+            fetchData({
+              sortBy: e.target.value,
+              time: formik.values.time,
+              limit: formik.values.limit
+            });
+          }}
+        >
+          {sortByOptions.map(entry => {
+            return (
+              <MenuItem key={entry} value={entry.toLowerCase()}>
+                {entry}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
 
-      <select
-        value={formik.values.time}
-        onChange={e => {
-          formik.values.time = e.target.value;
-          fetchData({
-            sortBy: formik.values.sortBy,
-            time: e.target.value,
-            limit: formik.values.limit
-          });
-        }}
-        disabled={formik.values.sortBy !== "top"}
-      >
-        {timeOptions.map(entry => {
-          return (
-            <option
-              key={entry}
-              value={entry.toLowerCase()}
-              defaultValue={entry === "Week"}
-            >
-              {entry}
-            </option>
-          );
-        })}
-      </select>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Time</InputLabel>
+        <Select
+          value={formik.values.time}
+          onChange={e => {
+            formik.values.time = e.target.value;
+            fetchData({
+              sortBy: formik.values.sortBy,
+              time: e.target.value,
+              limit: formik.values.limit
+            });
+          }}
+          disabled={formik.values.sortBy !== "top"}
+        >
+          {timeOptions.map(entry => {
+            return (
+              <MenuItem key={entry} value={entry.toLowerCase()}>
+                {entry}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
 
-      <select
-        value={formik.values.limit}
-        onChange={e => {
-          formik.values.limit = e.target.value;
-          fetchData({
-            sortBy: formik.values.sortBy,
-            time: formik.values.time,
-            limit: e.target.value
-          });
-        }}
-      >
-        {limitOptions.map(entry => {
-          return (
-            <option
-              key={entry}
-              value={Number.parseInt(entry)}
-              defaultValue={Number.parseInt(entry) === 25}
-            >
-              {entry}
-            </option>
-          );
-        })}
-      </select>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Limit</InputLabel>
+        <Select
+          value={formik.values.limit}
+          onChange={e => {
+            formik.values.limit = e.target.value;
+            fetchData({
+              sortBy: formik.values.sortBy,
+              time: formik.values.time,
+              limit: e.target.value
+            });
+          }}
+        >
+          {limitOptions.map(entry => {
+            return (
+              <MenuItem key={entry} value={Number.parseInt(entry)}>
+                {entry}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
     </div>
   );
 };
@@ -126,7 +138,7 @@ const Dashboard = () => {
         <p>Searching...</p>
       ) : (
         data.map(entry => {
-          return <DashboardCard entry={entry} />;
+          return <DashboardCard entry={entry} key={entry.id} />;
         })
       )}
     </div>
